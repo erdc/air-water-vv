e14d26d12691aa03b3fdbc5114e8af473f932dcc
ezve <e.zve@hrwallingford.com>
2014-11-13 15:46:46 +0000

    The crump weir cases for modular and non modular flow

---
+++ 2d/crump_weir/crump_weir_modular/ls_consrv_n.py
@@ -0,0 +1,48 @@
+from proteus import *
+from crump_weir import *
+from ls_consrv_p import *
+
+timeIntegrator  = ForwardIntegrator
+timeIntegration = NoIntegration
+
+femSpaces = {0:basis}
+
+subgridError      = None
+massLumping       = False
+numericalFluxType = DoNothing
+conservativeFlux  = None
+shockCapturing    = None
+
+fullNewtonFlag = True
+multilevelNonlinearSolver = Newton
+levelNonlinearSolver      = Newton
+
+nonlinearSmoother = None
+linearSmoother    = None
+
+matrix = SparseMatrix
+
+if useOldPETSc:
+    multilevelLinearSolver = PETSc
+    levelLinearSolver      = PETSc
+else:
+    multilevelLinearSolver = KSP_petsc4py
+    levelLinearSolver      = KSP_petsc4py
+
+if useSuperlu:
+    multilevelLinearSolver = LU
+    levelLinearSolver      = LU
+
+linear_solver_options_prefix = 'mcorr_'
+linearSolverConvergenceTest  = 'r-true'
+levelNonlinearSolverConvergenceTest  = 'r'
+
+tolFac = 0.0
+nl_atol_res = mcorr_nl_atol_res
+
+linTolFac = 0.0
+l_atol_res = 0.001*mcorr_nl_atol_res
+useEisenstatWalker = False
+
+maxNonlinearIts = 50
+maxLineSearches = 0
---
+++ 2d/crump_weir/crump_weir_nonmodular_V1/crump_weir_so.py
@@ -0,0 +1,38 @@
+from proteus.default_so import *
+import crump_weir
+
+if crump_weir.useOnlyVF:
+    pnList = [("twp_navier_stokes_p", "twp_navier_stokes_n"),
+              ("vof_p",               "vof_n")]
+else:
+    pnList = [("twp_navier_stokes_p", "twp_navier_stokes_n"),
+              ("vof_p",               "vof_n"),
+              ("ls_p",                "ls_n"),
+              ("redist_p",            "redist_n"),
+              ("ls_consrv_p",         "ls_consrv_n")]
+    
+    
+if crump_weir.useRANS > 0:
+    pnList.append(("kappa_p",
+                   "kappa_n"))
+    pnList.append(("dissipation_p",
+                   "dissipation_n"))
+name = "crump_weir_p" 
+
+if crump_weir.timeDiscretization == 'flcbdf':
+    systemStepControllerType = Sequential_MinFLCBDFModelStep
+    systemStepControllerType = Sequential_MinAdaptiveModelStep
+else:
+    systemStepControllerType = Sequential_MinAdaptiveModelStep
+
+needEBQ_GLOBAL = False
+needEBQ = False
+
+tnList = [0.0,crump_weir.dt_init]+[i*crump_weir.dt_fixed for i in range(1,crump_weir.nDTout+1)] 
+
+info = open("TimeList.txt","w")
+
+
+for time in tnList:
+    info.write(str(time)+"\n")
+info.close()
