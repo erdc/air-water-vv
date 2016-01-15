ddb951f48852a929f5252d29a357de1caf201ca0
Tristan de Lataillade <delataillade.tristan@gmail.com>
2016-01-15 16:09:46 +0000

    New validation case: free roll of floating caisson in 2D

---
+++ 2d/floating_caisson/roll_free/caisson2D_freeroll.py
@@ -0,0 +1,254 @@
+from proteus import Domain
+from proteus.mprans import SpatialTools as st
+import ode
+from math import *
+import numpy as np
+# ----- DOMAIN ----- #
+
+domain = Domain.PlanarStraightLineGraphDomain()
+
+waterLevel = 0.9
+
+# ----- SHAPES ----- #
+
+tank_dim = [5., 1.2]
+leftSponge = 2.
+rightSponge = 2.
+
+caisson_dim = [.3, .1]
+caisson_coords = [tank_dim[0]/2., waterLevel]
+inertia = 0.236
+width = 0.9
+caisson_mass = 15
+free_x = [0., 0., 0.]
+free_r = [0., 0., 1.]
+rotation = pi/12.
+
+tank = st.Tank2D(domain, tank_dim)
+tank.setSponge(left=leftSponge, right=rightSponge)
+left = right = False
+if leftSponge is not None: left = True
+if rightSponge is not None: right = True
+tank.setAbsorptionZones(left=left, right=right)
+
+
+caisson2D = st.Rectangle(domain, dim=caisson_dim, coords=caisson_coords)
+caisson2D.setRigidBody()
+caisson2D.setMass(caisson_mass)
+caisson2D.setConstraints(free_x=free_x, free_r=free_r)
+caisson2D.rotate(rotation)  # initial position for free oscillation
+caisson2D.It = inertia/caisson2D.mass/width
+caisson2D.setRecordValues(pos=True, rot=True, F=True, M=True)
+
+# ----- BOUNDARY CONDITIONS ----- #
+
+for bc in caisson2D.BC_list:
+    bc.setNoSlip()
+
+tank.BC.top.setOpenAir()
+tank.BC.bottom.setNoSlip()
+tank.BC.left.setNoSlip()
+tank.BC.right.setNoSlip()
+tank.BC.sponge.setParallelFlag0()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################
+# Numerical Options and other parameters #
+##########################################
+
+# nd = 2
+rho_0=998.2
+nu_0 =1.004e-6
+rho_1=1.205
+nu_1 =1.500e-5
+sigma_01=0.0
+g = [0., -9.81]
+# ------------------
+
+
+
+domain.MeshOptions.he = (caisson2D.dim[-1])/12.0 #coarse grid
+
+
+from math import *
+from proteus import MeshTools, AuxiliaryVariables
+import numpy
+import proteus.MeshTools
+from proteus import Domain
+from proteus.Profiling import logEvent
+from proteus.default_n import *
+from proteus.ctransportCoefficients import smoothedHeaviside
+from proteus.ctransportCoefficients import smoothedHeaviside_integral
+
+st.assembleDomain(domain)
+
+#----------------------------------------------------
+# Boundary conditions and other flags
+#----------------------------------------------------
+movingDomain=True
+checkMass=False
+applyCorrection=True
+applyRedistancing=True
+freezeLevelSet=True
+
+#----------------------------------------------------
+# Time stepping and velocity
+#----------------------------------------------------
+weak_bc_penalty_constant = 10.0/nu_0#Re
+dt_init=0.001
+T = 10
+nDTout= 40*T
+dt_out =  (T-dt_init)/nDTout
+runCFL = 0.33
+
+#----------------------------------------------------
+water_depth  = waterLevel
+
+#  Discretization -- input options
+useOldPETSc=False
+useSuperlu = not True
+spaceOrder = 1
+useHex     = False
+useRBLES   = 0.0
+useMetrics = 1.0
+useVF = 1.0
+useOnlyVF = False
+useRANS = 1 # 0 -- None
+            # 1 -- K-Epsilon
+            # 2 -- K-Omega, 1998
+            # 3 -- K-Omega, 1988
+# Input checks
+if spaceOrder not in [1,2]:
+    print "INVALID: spaceOrder" + spaceOrder
+    sys.exit()
+
+if useRBLES not in [0.0, 1.0]:
+    print "INVALID: useRBLES" + useRBLES
+    sys.exit()
+
+if useMetrics not in [0.0, 1.0]:
+    print "INVALID: useMetrics"
+    sys.exit()
+
+#  Discretization
+nd = 2
+if spaceOrder == 1:
+    hFactor=1.0
+    if useHex:
+	 basis=C0_AffineLinearOnCubeWithNodalBasis
+         elementQuadrature = CubeGaussQuadrature(nd,3)
+         elementBoundaryQuadrature = CubeGaussQuadrature(nd-1,3)
+    else:
+    	 basis=C0_AffineLinearOnSimplexWithNodalBasis
+         elementQuadrature = SimplexGaussQuadrature(nd,3)
+         elementBoundaryQuadrature = SimplexGaussQuadrature(nd-1,3)
+         #elementBoundaryQuadrature = SimplexLobattoQuadrature(nd-1,1)
+elif spaceOrder == 2:
+    hFactor=0.5
+    if useHex:
+	basis=C0_AffineLagrangeOnCubeWithNodalBasis
+        elementQuadrature = CubeGaussQuadrature(nd,4)
+        elementBoundaryQuadrature = CubeGaussQuadrature(nd-1,4)
+    else:
+	basis=C0_AffineQuadraticOnSimplexWithNodalBasis
+        elementQuadrature = SimplexGaussQuadrature(nd,4)
+        elementBoundaryQuadrature = SimplexGaussQuadrature(nd-1,4)
+
+
+# Numerical parameters
+ns_forceStrongDirichlet = False
+backgroundDiffusionFactor=0.01
+if useMetrics:
+    ns_shockCapturingFactor  = 0.5
+    ns_lag_shockCapturing = True
+    ns_lag_subgridError = True
+    ls_shockCapturingFactor  = 0.5
+    ls_lag_shockCapturing = True
+    ls_sc_uref  = 1.0
+    ls_sc_beta  = 1.5
+    vof_shockCapturingFactor = 0.5
+    vof_lag_shockCapturing = True
+    vof_sc_uref = 1.0
+    vof_sc_beta = 1.5
+    rd_shockCapturingFactor  = 0.5
+    rd_lag_shockCapturing = False
+    epsFact_density    = 3.0
+    epsFact_viscosity  = epsFact_curvature  = epsFact_vof = epsFact_consrv_heaviside = epsFact_consrv_dirac = epsFact_density
+    epsFact_redistance = 0.33
+    epsFact_consrv_diffusion = 1.0
+    redist_Newton = True
+    kappa_shockCapturingFactor = 0.5
+    kappa_lag_shockCapturing = True
+    kappa_sc_uref = 1.0
+    kappa_sc_beta = 1.5
+    dissipation_shockCapturingFactor = 0.5
+    dissipation_lag_shockCapturing = True
+    dissipation_sc_uref = 1.0
+    dissipation_sc_beta = 1.5
+else:
+    ns_shockCapturingFactor  = 0.9
+    ns_lag_shockCapturing = True
+    ns_lag_subgridError = True
+    ls_shockCapturingFactor  = 0.9
+    ls_lag_shockCapturing = True
+    ls_sc_uref  = 1.0
+    ls_sc_beta  = 1.0
+    vof_shockCapturingFactor = 0.9
+    vof_lag_shockCapturing = True
+    vof_sc_uref  = 1.0
+    vof_sc_beta  = 1.0
+    rd_shockCapturingFactor  = 0.9
+    rd_lag_shockCapturing = False
+    epsFact_density    = 1.5
+    epsFact_viscosity  = epsFact_curvature  = epsFact_vof = epsFact_consrv_heaviside = epsFact_consrv_dirac = epsFact_density
+    epsFact_redistance = 0.33
+    epsFact_consrv_diffusion = 10.0
+    redist_Newton = False#True
+    kappa_shockCapturingFactor = 0.9
+    kappa_lag_shockCapturing = True#False
+    kappa_sc_uref  = 1.0
+    kappa_sc_beta  = 1.0
+    dissipation_shockCapturingFactor = 0.9
+    dissipation_lag_shockCapturing = True#False
+    dissipation_sc_uref  = 1.0
+    dissipation_sc_beta  = 1.0
+
+ns_nl_atol_res = max(1.0e-12,0.00001*domain.MeshOptions.he**2)
+vof_nl_atol_res = max(1.0e-12,0.00001*domain.MeshOptions.he**2)
+ls_nl_atol_res = max(1.0e-12,0.00001*domain.MeshOptions.he**2)
+mcorr_nl_atol_res = max(1.0e-12,0.000001*domain.MeshOptions.he**2)
+rd_nl_atol_res = max(1.0e-12,0.01*domain.MeshOptions.he)
+kappa_nl_atol_res = max(1.0e-12,0.00001*domain.MeshOptions.he**2)
+dissipation_nl_atol_res = max(1.0e-12,0.00001*domain.MeshOptions.he**2)
+mesh_nl_atol_res = max(1.0e-12,0.00001*domain.MeshOptions.he**2)
+
+#turbulence
+ns_closure=0 #1-classic smagorinsky, 2-dynamic smagorinsky, 3 -- k-epsilon, 4 -- k-omega
+
+if useRANS == 1:
+    ns_closure = 3
+elif useRANS >= 2:
+    ns_closure == 4
+
+def twpflowPressure_init(x, t):
+    p_L = 0.0
+    phi_L = tank.dim[nd-1] - waterLevel
+    phi = x[nd-1] - waterLevel
+    return p_L -g[nd-1]*(rho_0*(phi_L - phi)+(rho_1 -rho_0)*(smoothedHeaviside_integral(epsFact_consrv_heaviside*domain.MeshOptions.he,phi_L)
+                                                         -smoothedHeaviside_integral(epsFact_consrv_heaviside*domain.MeshOptions.he,phi)))
+
+tank.BC.top.p_dirichlet = twpflowPressure_init
+
