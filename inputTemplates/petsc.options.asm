fb3eb0899cfd0048ac1c7a31bc42908625fd95db
cekees <cekees@gmail.com>
2016-01-30 01:39:01 -0600

    made conservation correction more robust

--- inputTemplates/petsc.options.asm
+++ inputTemplates/petsc.options.asm
@@ -3,7 +3,7 @@
 -ncls_ksp_type   gmres -ncls_pc_type asm
 -vof_ksp_type    gmres -vof_pc_type asm
 -rdls_ksp_type   gmres -rdls_pc_type asm -rdls_pc_asm_type basic -rdls_ksp_gmres_modifiedgramschmidt -rdls_ksp_gmres_restart 300 -rdls_ksp_knoll -rdls_sub_ksp_type preonly -rdls_sub_pc_type lu -rdls_ksp_max_it 2000
--mcorr_ksp_type  cg -mcorr_pc_type asm
+-mcorr_ksp_type  cg -mcorr_pc_type asm -mcorr_pc_asm_type basic -mcorr_sub_pc_type lu -mcorr_ksp_max_it 2000
 -mesh_ksp_type cg -mesh_pc_type asm -mesh_pc_asm_type basic -mesh_ksp_max_it 2000
 -mesh_sub_ksp_type preonly -mesh_ksp_knoll -mesh_sub_pc_type lu
 -log_summary
