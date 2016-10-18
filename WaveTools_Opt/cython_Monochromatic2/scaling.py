120e76f42bb2b6b2bba7d9b9eab2e04e022c1a5a
adimako <asd@hrwallingford.com>
2016-10-18 17:16:45 +0100

    2nd optimisation effort goes with 16ca87334581d8fca9641d0bd6e1eff7746d0618 commit of proteus

---
+++ WaveTools_Opt/cython_Monochromatic2/scaling.py
@@ -0,0 +1,34 @@
+fid1 = open("parallel_12/tank_so.log","r")
+fid2 = open("serial/tank_so.log","r")
+
+serial = fid2.readlines()
+parallel = fid1.readlines()
+
+serialS = 0.
+serialE = 0.
+id = 0
+for line in serial:
+    if "Solving over interval" in line and id == 0:
+        serialS = float(line[1:9])
+        id = 1
+    if "Closing Archive" in line:
+        serialE = float(line[1:9])
+print "serialS,serialE"
+
+parallelS = 0.
+parallelE = 0.
+id = 0
+for line in parallel:
+    if "Solving over interval" in line and id == 0:
+        parallelS = float(line[1:9])
+        id = 1
+    if "Closing Archive" in line:
+        parallelE = float(line[1:9])
+
+print "Serial start time=" +str(serialS)
+print "Serial end time=" +str(serialE)
+print "Parallel start time=" +str(parallelS)
+print "Parallel start time=" +str(parallelE)
+print "Number of processors=12"
+scalEff = (serialE-serialS)/(parallelE-parallelS)/12
+print "Scaling efficiency="+str(scalEff)
