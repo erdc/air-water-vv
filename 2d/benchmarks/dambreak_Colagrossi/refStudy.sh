7b671dca947c956a56bf07a83723c39dd62e85dd
Chris Kees <cekees@gmail.com>
2017-08-28 12:03:23 -0500

    better failure detection

--- 2d/benchmarks/dambreak_Colagrossi/refStudy.sh
+++ 2d/benchmarks/dambreak_Colagrossi/refStudy.sh
@@ -12,6 +12,6 @@ parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.0125 cfl=0.9 T=1.0" -O petsc/pe
 # parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.025 cfl=0.9 T=1.0 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_r4
 # parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.0125 cfl=0.9 T=1.0 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_r5
 echo "Iteration Statistics" > stats.txt
-for f in *_r*/*.log; do echo $f >> stats.txt; grep "converged= True" $f >> stats.txt; grep failed $f; done
+for f in *_r*/*.log; do echo $f >> stats.txt; grep "converged= True" $f >> stats.txt; grep -l -c failed $f; done
 cat stats.txt
 ./avg.py stats.txt
--- 2d/benchmarks/dambreak_Colagrossi/refStudyF.sh
+++ 2d/benchmarks/dambreak_Colagrossi/refStudyF.sh
@@ -5,12 +5,12 @@ parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.1 dt_fixed=0.01 fixed_step=True
 parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.05 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1" -O petsc/petsc.options.schur -D pcd_rf3
 parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.025 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1" -O petsc/petsc.options.schur -D pcd_rf4
 parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.0125 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1" -O petsc/petsc.options.schur -D pcd_rf5
-# parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.4 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.2 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf0
-# parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.2 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.2 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf1
-# parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.1 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.2 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf2
-# parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.05 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf3
-# parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.025 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf4
-# parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.0125 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf5
+parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.4 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.2 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf0
+parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.2 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.2 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf1
+parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.1 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.2 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf2
+parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.05 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf3
+parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.025 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf4
+parun dambreak_Colagrossi_so.py -l 5 -v -C "he=0.0125 dt_fixed=0.01 fixed_step=True T=1.0 dt_out=0.1 schur_solver='selfp_petsc'" -O petsc/petsc.options.schur.selfp_petsc -D selfp_rf5
 echo "Iteration Statistics" > statsf.txt
 for f in *_rf*/*.log; do echo $f >> statsf.txt; grep "converged= True" $f >> statsf.txt; done
 cat statsf.txt
