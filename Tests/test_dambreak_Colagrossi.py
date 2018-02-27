a49e9ac24dbdc6cb4864d91295c2f82782fa0505
Pedro Otiñar Morillas <Pedrohrw>
2018-02-27 11:43:36 +0000

    fix path test_Colagrossi

--- Tests/test_dambreak_Colagrossi.py
+++ Tests/test_dambreak_Colagrossi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-os.chdir('/home/travis/build/proteus/air-water-vv/2d/benchmarks/dambreak_Colagrossi')
+os.chdir('/home/travis/build/erdc/proteus/air-water-vv/2d/benchmarks/dambreak_Colagrossi')
 import pytest
 from proteus.iproteus import *
 from proteus import Comm
