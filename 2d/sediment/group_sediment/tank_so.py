b6fa0b8eb7b6cbe6663442c6c9e0b1c7d4339ccd
Brittany <breizh@live.unc.edu>
2019-07-12 09:46:39 -0500

    corrected _so.py files

--- 2d/sediment/group_sediment/tank_so.py
+++ 2d/sediment/group_sediment/tank_so.py
@@ -29,6 +29,7 @@ for BC in ct.domain.bc:
 
 from proteus.SplitOperator import Sequential_FixedStep_Simple, defaultSystem
 if ct.sedimentDynamics:
+    PINIT_model=9
     pnList = [("vos_p",               "vos_n"),#0
               ("vof_p",               "vof_n"),#1
               ("ls_p",                "ls_n"),#2
@@ -37,32 +38,28 @@ if ct.sedimentDynamics:
               ("threep_navier_stokes_sed_p", "threep_navier_stokes_sed_n"),#5
               ("twp_navier_stokes_p", "twp_navier_stokes_n"),#6
               ("pressureincrement_p", "pressureincrement_n"),#7
-              ("pressure_p", "pressure_n")]#8
-    
+              ("pressure_p", "pressure_n"),#8
+              ("pressureInitial_p", "pressureInitial_n")]#9
 else:
+    PINIT_model=7
     pnList = [("vof_p",               "vof_n"),#0
               ("ls_p",                "ls_n"),#1
               ("redist_p",            "redist_n"),#2
               ("ls_consrv_p",         "ls_consrv_n"),#3
               ("twp_navier_stokes_p", "twp_navier_stokes_n"),#4
               ("pressureincrement_p", "pressureincrement_n"),#5
-              ("pressure_p", "pressure_n")]#6
+              ("pressure_p", "pressure_n"),#6
+              ("pressureInitial_p", "pressureInitial_n")]#7
 
 if ct.useRANS > 0:
     pnList.append(("kappa_p",
-                   "kappa_n"))#7 or #9
+                   "kappa_n"))
     pnList.append(("dissipation_p",
-                   "dissipation_n"))#8 or #10
-
-pnList.append(("pressureInitial_p",
-                   "pressureInitial_n"))#7 or #9
-
-        
-              
+                   "dissipation_n"))
 name = "tank"
 
 #modelSpinUpList = [ct.VOF_model, ct.LS_model, ct.V_model, ct.PINIT_model]
-modelSpinUpList = [ct.PI_model]
+modelSpinUpList = [PINIT_model]
 
 class Sequential_MinAdaptiveModelStepPS(Sequential_MinAdaptiveModelStep):
     def __init__(self,modelList,system=defaultSystem,stepExact=True):
@@ -73,8 +70,8 @@ class Sequential_MinAdaptiveModelStepPS(Sequential_MinAdaptiveModelStep):
 #    def __init__(self,modelList,system=defaultSystem,stepExact=True):
 #        Sequential_FixedStep.__init__(self,modelList,system,stepExact)
 #        self.modelList = modelList[:len(pnList)-1]
-#dt_system_fixed = ct.dt_fixed
 
+dt_system_fixed = ct.dt_fixed
 systemStepControllerType = Sequential_MinAdaptiveModelStepPS
 
 needEBQ_GLOBAL = False
