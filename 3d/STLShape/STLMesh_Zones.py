a25488b3852ec93252ca45c113fd894b04e187f2
BranocRichards <branoc.richards@gmail.com>
2019-12-13 22:55:31 +0000

    Working Generation Zones files

--- 3d/STLShape/STLMesh_Zones.py
+++ 3d/STLShape/STLMesh_Zones.py
@@ -8,8 +8,6 @@ from proteus.Profiling import logEvent
 from proteus import MeshTools as mt
 from proteus.ctransportCoefficients import smoothedHeaviside
 from proteus.ctransportCoefficients import smoothedHeaviside_integral
-import pdb
-
 
 
 #   ____            _            _      ___        _   _
@@ -34,13 +32,12 @@ context_options += [
    ("nu_1", 1.5e-5, "Air kinematic viscosity m/sec^2"),
    ("sigma_01", 0., "Surface tension"),
    ("g", np.array([0, -9.81, 0.]), "Gravitational acceleration vector"),
-    ("U", np.array([0.1,0,0]), "inlet velocity"),
-    ("rampTime", 0.1, "inlet ramp"),
+   ("U", np.array([0.1,0,0]), "inlet velocity"),
    ("outlet_level", 2.5, "outlet level")
-    
 
 ]
-# wave options
+
+#wave options
 context_options +=[
     ("waveHeight", 0.25, "Wave Height"), 
     ("period", 2., "Wave Period"),
@@ -55,11 +52,11 @@ context_options += [
     ("dt_fixed", None, "Value of maximum time step"),
     ("dt_output", 0.1, "number of saves per second"),
     ("cfl", 0.5 ,"Target CFL value")
-    ]
 
+]
+
+#mesh refinement
 context_options += [
-   
-	# mesh refinement
     ("he", 1., "Set characteristic element size"),
     # numerical options
     ("genMesh", True, "True: generate new mesh every time. False: do not generate mesh if file 	    exists"),
@@ -70,14 +67,8 @@ context_options += [
     ("movingDomain", False, "True/False")
 ]
 
-
-
-
 # instantiate context options
 opts=Context.Options(context_options)
-
-
-
 
 #  ____                        _
 # |  _ \  ___  _ __ ___   __ _(_)_ __
@@ -89,13 +80,13 @@ opts=Context.Options(context_options)
 
 domain = Domain.PiecewiseLinearComplexDomain()
 
-##WAVE##
+# Wave
 g = np.array([0., -9.81, 0.])
 nu_0 = 1.004e-6
 period=opts.period
 
 wave = wt.MonochromaticWaves(period=period,
-                            waveHeight=opts.waveHeight,
+                             waveHeight=opts.waveHeight,
                              mwl=opts.mwl,
                              depth=opts.mwl,
                              g=g,
@@ -106,18 +97,9 @@ wave = wt.MonochromaticWaves(period=period,
 #ShapeSTL
 SG=st.ShapeSTL(domain,'NWT.stl')
 
-#Tank3D
-#SG = st.Tank3D(domain=domain, dim=[16., 5., 20.])
-#SG.translate(trans=[-11., 0., 0.])
-#SG.regions=np.array([[0.,3.,10.]])
-
 boundaryTags= SG.boundaryTags
 
-#current=wt.SteadyCurrent(U=opts.U,
-#                         mwl=opts.outlet_level,
-#                         rampTime=opts.rampTime)
-
-SG.regions=np.array([[-13., 2.5, 0.], [-3., 2.5, 0.], [9., 2.5, 0.]])                   #To Change?
+SG.regions=np.array([[-13., 2.5, 0.], [-3., 2.5, 0.], [9., 2.5, 0.]]
 SG.regionFlags=np.array([1, 2, 3])
 
 dragAlpha = 5*(2*np.pi/period)/nu_0
@@ -125,44 +107,23 @@ smoothing = 3. * opts.he
 nd = domain.nd
 xTop = max(SG.vertices[:,1])
 
-#SG.setSponge(x_n=2., x_p=4., y_p=0., y_n=0.)
-
 ##Relaxation Zones
-#Tank3D
-#SG.setGenerationZones(dragAlpha=dragAlpha,
-#                           smoothing=smoothing,
-#                           waves=wave,
-#                           allSponge=False,
-#                           x_n=True,
-#                           x_p=False,
-#                           y_n=False,
-#                           y_p=False
-#                           )
-
-#ShapeSTL
 SG.setGenerationZones(flags=1,
                       epsFact_solid=2.,
                       center=np.array([-13., 2.5, 0.]),
                       orientation=np.array([1., 0., 0.]),
                       waves=wave,
                       dragAlpha=dragAlpha,
+                      vert_axis=1,
+                      smoothing=smoothing,
                       )
 
-#Tank3D
-#SG.setAbsorptionZones(dragAlpha=dragAlpha,
-#                           allSponge=False,
-#                           x_n=False,
-#                           x_p=True,
-#                           y_n=False,
-#                           y_p=False
-#                           )
-
-#ShapeSTL
 SG.setAbsorptionZones(flags=3,
                       epsFact_solid=4.,
                       center=np.array([9., 2.5, 0.]),
                       orientation=np.array([-1., 0., 0.]),
-                      dragAlpha=dragAlpha
+                      dragAlpha=dragAlpha,
+                      vert_axis=1
                       )
 
 SG.BC['Top_Gen'].setAtmosphere(orientation=np.array([0,+1,0]))
@@ -179,35 +140,28 @@ SG.BC['Concrete1'].setFreeSlip()
 SG.BC['Inlet1'].setNonMaterial()
 SG.BC['Outlet1'].setNonMaterial()
 
-#TESTING
-#SG.BC['Inlet_Gen'].setFreeSlip()
-#SG.BC['Outlet_Abs'].setFreeSlip()
-
 SG.BC['Inlet_Gen'].setUnsteadyTwoPhaseVelocityInlet(wave=wave, 
-                                                 vert_axis=1,
-					         smoothing=3.*opts.he,
-						 orientation=np.array([-1.,0.,0.]),
-                                                 vof_air=1.,
-                                                 vof_water=0.)
+                                                    vert_axis=1,
+                                                    smoothing=3.*opts.he,
+                                                    orientation=np.array([-1.,0.,0.]),
+                                                    vof_air=1.,
+                                                    vof_water=0.)
 
 SG.BC['Outlet_Abs'].setHydrostaticPressureOutletWithDepth(seaLevel= opts.mwl,
-                                                       rhoUp=opts.rho_1,
-                                                       rhoDown=opts.rho_0,
-                                                       g=opts.g,
-                                                       refLevel=xTop,
-                                                       smoothing=smoothing,
-                                                       orientation=np.array([1.,0.,0.]),
-                                                       vert_axis=1)
-
-
-
+                                                          rhoUp=opts.rho_1,
+                                                          rhoDown=opts.rho_0,
+                                                          g=opts.g,
+                                                          refLevel=xTop,
+                                                          smoothing=smoothing,
+                                                          orientation=np.array([1.,0.,0.]),
+                                                          vert_axis=1)
 
 #  ___       _ _   _       _    ____                _ _ _   _
 # |_ _|_ __ (_) |_(_) __ _| |  / ___|___  _ __   __| (_) |_(_) ___  _ __  ___
 #  | || '_ \| | __| |/ _` | | | |   / _ \| '_ \ / _` | | __| |/ _ \| '_ \/ __|
 #  | || | | | | |_| | (_| | | | |__| (_) | | | | (_| | | |_| | (_) | | | \__ \
 # |___|_| |_|_|\__|_|\__,_|_|  \____\___/|_| |_|\__,_|_|\__|_|\___/|_| |_|___/
-# Initial Conditions
+#
 
 vert_axis = np.where(abs(opts.g)>0)[0][0]
 
