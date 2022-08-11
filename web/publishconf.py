c61ec2db6ee6baa5fc469ef80b7095b8049ad332
Chase Hamilton <chase.o.hamilton@erdc.dren.mil>
2022-08-11 13:45:03 -0500

    'all references to "erdc-cm" changed to "erdc"'

--- web/publishconf.py
+++ web/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://erdc-cm.github.io/air-water-vv'
+SITEURL = 'https://erdc.github.io/air-water-vv'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
